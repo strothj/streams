2c298f4ebf687cfc29346925fa73b1458e8f7790
strothj <jason@jasons.io>
2016-10-25 20:23:07 -0400

    Added Writable stream

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 var Alphabet = require('./alphabet');
+var Cache = require('./cache');
 var alpha = new Alphabet();
-alpha.on('data', function(chunk) {
-  console.log(chunk.toString());
-});
+var cache = new Cache('alpha1');
+var cache2 = new Cache('alpha2');
+
+alpha.pipe(cache);
+(new Alphabet).pipe(cache2);
+
+function logCache() {
+  console.log('Cache store:');
+  for (var key in Cache.store) {
+    console.log(key, ':', Cache.store[key]);
+  }
+}
+
+cache.on('finish', logCache);
+cache2.on('finish', logCache);
